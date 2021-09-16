8235026edfa1d01df18607497ce6a0a7ac1a1a0f
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2021-09-16 19:43:17 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b72ebbced687c9c3f0e5ad45bd20d7e7'),
+    },
+  },
+});
