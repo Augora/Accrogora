b05aee2f00f5b26061fe1bda099a5d5aa2e00a92
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2021-09-28 00:54:17 +0200

    Tries another server config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8346),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b72ebbced687c9c3f0e5ad45bd20d7e7'),
-    },
-  },
+  // port: env.int('PORT', 8346),
+  url: env('', 'http://accrogora.herokuapp.com'),
+  // admin: {
+  //   auth: {
+  //     secret: env('ADMIN_JWT_SECRET', 'b72ebbced687c9c3f0e5ad45bd20d7e7'),
+  //   },
+  // },
 });
