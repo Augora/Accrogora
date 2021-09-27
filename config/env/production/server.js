cb094964d975f092efb6778fb823384a5b6411a2
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2021-09-28 00:48:01 +0200

    Removes unused folders, try to get port on twitch callback

--- config/env/production/server.js
+++
@@ -1,4 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  url: env('https://accrogora.herokuapp.com'),
-});
--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8346),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b72ebbced687c9c3f0e5ad45bd20d7e7'),
