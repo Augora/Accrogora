7c6a80e22f1dbd716cc00beadd2224420ac39640
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2021-09-28 01:21:34 +0200

    Restore admin auth secret

--- config/server.js
+++ config/server.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   // host: env('HOST', '0.0.0.0'),
   // port: env.int('PORT', 8346),
   url: env('', 'https://accrogora.herokuapp.com'),
-  // admin: {
-  //   auth: {
-  //     secret: env('ADMIN_JWT_SECRET', 'b72ebbced687c9c3f0e5ad45bd20d7e7'),
-  //   },
-  // },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b72ebbced687c9c3f0e5ad45bd20d7e7'),
+    },
+  },
 });
